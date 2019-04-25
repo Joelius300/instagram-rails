594e23cec14bdf0c2bd7a56c4d2bc092b51f7be9
Joel Liechti <jli10828@iet-gibb.ch>
2019-04-25 09:37:33 +0200

    Start implementing avatars (show/edit user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def avatar_url user
+    img = "pb#{user.id}.jpg"
+    if FileTest.exist?("#{::Rails.root}/public/images/#{img}")
+      image-url(img)
+    else
+      image-url("templatePB.png")
+    end
+  end
 end
