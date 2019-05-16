db8f411b029ec1fdca02761823b6455065179fbd
Joel Liechti <jli10828@iet-gibb.ch>
2019-05-16 09:35:14 +0200

    Add Photo and Post Model, add gems for file-upload, start with PostController

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_post
+
+  end
+
+  def post_params
+
+  end
+end
