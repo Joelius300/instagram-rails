ff1207d1b3d49cf0d4b1f3c38c72ae1841b6dcbf
Joel Liechti <jli10828@iet-gibb.ch>
2019-05-23 08:19:02 +0200

    Finish AB5, finish post_controller, add posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,28 @@
 class PostsController < ApplicationController
   def index
-
+      @posts = Post.all.limit(10).includes(:photos)
+      @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+        if params[:images]
+            params[:images].each do |img|
+                @post.photos.create(image: img)
+            end
+        end
 
+        redirect_to posts_path
+        flash[:notice] = "Saved ..."
+    else
+        flash[:alert] = "Something went wrong ..."
+        redirect_to posts_path
+    end
   end
 
   def show
-
+    @photos = @post.photos
   end
 
   def destroy
@@ -18,10 +32,14 @@ class PostsController < ApplicationController
   private
 
   def find_post
+    @post = Post.find_by id: params[:id]
 
+    return if @post
+    flash[:danger] = "Post doesn't exist!"
+    redirect_to root_path
   end
 
   def post_params
-
+    params.require(:post).permit :content
   end
 end
